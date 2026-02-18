5ecd93e91b587f3748649c7995f2f9f0d04b219c
Robert <robertaraya382@gmail.com>
2026-02-17 22:11:20 -0600

    Paso 1 Correcion de KoinHelper

--- shared/src/commonMain/kotlin/compose/project/demo/composedemo/di/KoinHelper.kt
+++ shared/src/commonMain/kotlin/compose/project/demo/composedemo/di/KoinHelper.kt
@@ -1,7 +1,7 @@
 package compose.project.demo.composedemo.di
 
-import org.koin.core.context.startKoin
 import org.koin.core.KoinApplication
+import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
 fun initKoin(config: KoinAppDeclaration? = null): KoinApplication {
